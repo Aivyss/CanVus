80e1e990204868c2a6f4f5b6c6e0468cc85587e1
Aivyss <hklee6417@gmail.com>
2021-01-16 21:47:35 +0900

    DrawingController 수정

--- CanVus/src/main/java/com/canvus/app/drawing/controller/DrawingController.java
+++ CanVus/src/main/java/com/canvus/app/drawing/controller/DrawingController.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.canvus.app.drawing.service.DrawingService;
 import com.canvus.app.drawing.vo.DrawingRoomVO;
@@ -32,13 +34,17 @@ public class DrawingController {
 	 * @return
 	 */
 	@RequestMapping(value="/enterRoom", method=RequestMethod.POST)
-	public String enterRoom(DrawingRoomVO drawingRoom, HttpSession session) {
-		boolean check = drawingService.enterRoom(drawingRoom);
+	public String enterRoom(DrawingRoomVO roomInfo, HttpSession session,
+			RedirectAttributes redirectAttributes) {
+		
+		boolean check = drawingService.enterRoom(roomInfo);
 		String url ="redirect:/main";
 		
 		if (check) {
-			session.setAttribute("roomId", drawingRoom.getRoomId());
-			url = "redirect:/drawing/drawingRoom";
+			List<Map<String, String>> userList = drawingService.getUserList(roomInfo.getRoomId());
+			redirectAttributes.addAttribute("roomInfo", roomInfo);
+			redirectAttributes.addAttribute("userList", userList);
+			url = "redirect:/drawing/room";
 		} else {
 			session.setAttribute("alert", "방 비밀번호 입력 오류");
 		}
@@ -52,12 +58,22 @@ public class DrawingController {
 	 * @return
 	 */
 	@RequestMapping(value="/createRoom", method=RequestMethod.POST)
-	public String createRoom(DrawingRoomVO roomInfo, Model model, HttpSession session) {
+	public String createRoom(DrawingRoomVO roomInfo, Model model, HttpSession session, RedirectAttributes redirectAttributes) {
 		DrawingRoomVO result = drawingService.createRoom(roomInfo, session);
 		List<Map<String, String>> userList = drawingService.getUserList(result.getRoomId());
 		
+		redirectAttributes.addAttribute("roomInfo", result);
+		redirectAttributes.addAttribute("userList", userList);
 		
-		model.addAttribute("roomInfo", result);
+		return "redirect:/drawing/room";
+	}
+	
+	@RequestMapping(value="/room", method=RequestMethod.GET)
+	public String room(@RequestParam("roomInfo") DrawingRoomVO roomInfo, 
+			@RequestParam("userList") List<Map<String, String>> userList,
+			Model model) {
+		
+		model.addAttribute("roomInfo", roomInfo);
 		model.addAttribute("userList", userList);
 		
 		return "drawing/room";
