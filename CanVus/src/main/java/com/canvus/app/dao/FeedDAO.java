da3fa08ba8c67e5acfe289060ff5d2802c4b67df
Aivyss <hklee6417@gmail.com>
2021-02-19 01:53:21 +0900

    다시 주석삭제(실수로 어맨드커밋 잘못씀)

--- CanVus/src/main/java/com/canvus/app/dao/FeedDAO.java
+++ CanVus/src/main/java/com/canvus/app/dao/FeedDAO.java
@@ -159,33 +159,16 @@ public class FeedDAO {
 			FeedDrawingsMapper mapper2 = session.getMapper(FeedDrawingsMapper.class); // 차후 진행
 			bundle = mapper1.selectFeedBundle(rb, user_id);
 
-			// 이해할 수 없는 잡버그로 주석처리
 			for (FeedComponentVO one : bundle) {
 				List<String> pictureURLs = new ArrayList<>();
-				log.info(one.getFeed_id());
 				List<FeedDrawingsVO> pictures = mapper2.selectFeedBundle(one.getFeed_id());
 
 				for (FeedDrawingsVO picture : pictures) {
 					pictureURLs.add(picture.getPage_file_output());
 				}
-				log.info(pictureURLs.toString());
 
 				one.setPictures(pictureURLs);
-				pictureURLs = null;
-				pictures = null;
 			}
-
-//			for (int i=0; i<bundle.size(); i++) {
-//				List<String> pictureURLs = new ArrayList<>();
-//				List<FeedDrawingsVO> pictures = mapper2.selectFeedBundle(bundle.get(i).getFeed_id());
-//
-//				for (int j=0; j<pictures.size(); j++) {
-//
-//					pictureURLs.add(pictures.get(j).getPage_file_output());
-//				}
-//
-//				bundle.get(i).setPictures(pictureURLs);
-//			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			log.info("피드 번들 가져오기 sql오류");
